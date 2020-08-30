d61099c953536de221cf03c91f1ffb5115cc62be
Andrey Shklyaruk <13mailg@gmail.com>
2020-08-30 22:51:19 +0300

    Fix styles

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
     <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
     <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
-  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+  styles: ['button { position: absolute; width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
 
